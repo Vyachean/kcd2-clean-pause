bcf84f7c4762500a03878371bc9897a7c7e37db6
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-09-03 18:15:14 +0400

    diag: start Steam pause-state observer with ASI

--- native/src/asi_entry.cpp
+++ native/src/asi_entry.cpp
@@ -1,5 +1,6 @@
 #include "clean_pause_native.h"
 #include "process_guard.h"
+#include "steam_pause_state_probe.h"
 
 #include <windows.h>
 
@@ -8,10 +9,13 @@ BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID)
     switch (reason) {
     case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls(instance);
-        if (clean_pause::AcquireProcessGuard())
+        if (clean_pause::AcquireProcessGuard()) {
             clean_pause::Start(instance);
+            clean_pause::StartSteamPauseStateProbe(instance);
+        }
         break;
     case DLL_PROCESS_DETACH:
+        clean_pause::StopSteamPauseStateProbe();
         clean_pause::Stop();
         break;
     default:
